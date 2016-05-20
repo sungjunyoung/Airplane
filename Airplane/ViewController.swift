d18da5e6d2560224abf5f2b22bca13288e4588ac
wnsdud1861 <wnsdud1861@gmail.com>
2016-05-20 12:20:19 +0900

    textile data load / when keyboard appear view up and disappear when click anywhere

--- Airplane/ViewController.swift
+++ Airplane/ViewController.swift
@@ -8,27 +8,56 @@
 
 import UIKit
 
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
 class ViewController: UIViewController{
     @IBOutlet weak var signinBtn: UIButton!
     @IBOutlet weak var signupBtn: UIButton!
-    @IBOutlet weak var tempText: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil);
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil);
+        //키보드 뜨는지
+        self.hideKeyboardWhenTappedAround()
         
         let path = NSBundle.mainBundle().pathForResource("userdata", ofType: "txt")
-        var text = String(contentsOfFile: path!, encoding: NSUTF8StringEncoding, error: nil)
         
-        tempText.text = text
+        var fileContents: String? = nil
+        do {
+            fileContents = try String(contentsOfFile: path!, encoding:NSUTF8StringEncoding)
+        } catch _ as NSError {
+                print("error!")
+        }
+        //data 는 fileContents에 담겨있습니다.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    func keyboardWillShow(sender: NSNotification) {
+        self.view.frame.origin.y = -150
+    }
+    
+    func keyboardWillHide(sender: NSNotification) {
+        self.view.frame.origin.y = 0
+    }
+    
+  
 
 }
 

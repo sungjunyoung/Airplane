59a79f0e2d5cd678393332c948785b97be5a6d0b
wnsdud1861 <wnsdud1861@gmail.com>
2016-06-03 00:15:09 +0900

    회원탈퇴 구현

--- Airplane/SettingViewController.swift
+++ Airplane/SettingViewController.swift
@@ -44,6 +44,9 @@ class SettingViewController : UIViewController{
         
     }
     
+    @IBAction func signOut(sender: AnyObject) {
+        signOutAlert()
+    }
     
     func passwordAlert(){
         let alert = UIAlertController(title: "비밀번호 불일치", message: "비밀번호가 일치하는지 확인해주세요", preferredStyle: UIAlertControllerStyle.Alert)
@@ -61,6 +64,31 @@ class SettingViewController : UIViewController{
         let cancel = UIAlertAction(title: "확인", style: UIAlertActionStyle.Cancel, handler: nil)
         
         alert.addAction(cancel)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func signOutAlert(){
+        let alert = UIAlertController(title: "회원탈퇴", message: "정말 회원탈퇴를 하시겠습니까?", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let yes = UIAlertAction(title: "예", style: UIAlertActionStyle.Default){
+            (action:UIAlertAction)->Void in print("OK")
+            let toDeleteUser = UserManager.nowUser
+            var counter = 0
+            for user in UserManager.userList{
+                if user.email == toDeleteUser.email{
+                    UserManager.userList.removeAtIndex(counter)
+                    break
+                }
+                counter += 1
+            }
+            UserManager.deleteUserInHashTable(toDeleteUser)
+            self.performSegueWithIdentifier("segSignOut", sender:self)
+        }
+        alert.addAction(yes)
+        
+        let no = UIAlertAction(title: "아니요", style: UIAlertActionStyle.Cancel, handler: nil)
+        alert.addAction(no)
         
         self.presentViewController(alert, animated: true, completion: nil)
     }
--- Airplane/UserManager.swift
+++ Airplane/UserManager.swift
@@ -34,6 +34,8 @@ class UserManager{
 
         return sum
     }
+    
+    //이름 해시 인덱스를 리턴하는 해싱 함수
     static func nameHashingFunc(name:String) -> Int{
         var sum = 0
         for character in name.utf8{
@@ -43,6 +45,17 @@ class UserManager{
         sum = sum % 1000
         
         return sum
+    }
+    
+    
+    static func deleteUserInHashTable(user:User){
+        let emailindex:Int = emailHashingFunc(user.email)
+        let email = user.email
+        emailHashTable[emailindex].removeAtIndex(emailBinarySearch(emailHashTable[emailindex], key:email))
+        
+        let nameIndex:Int = nameHashingFunc(user.name)
+        let name = user.name
+        nameHashTable[nameIndex].removeAtIndex(nameBinarySearch(nameHashTable[nameIndex], key:name))
     }
     
     //이메일 해시테이블을 검색하여 해당 유저를 리턴해주는 함수
@@ -55,6 +68,8 @@ class UserManager{
             return emailHashTable[index][emailBinarySearch(emailHashTable[index], key: email)]
         }
     }
+    
+    //유저의 정보를 해시테이블에 업데이트 해주는 함수
     static func updateUserInfo(user:User){
         let index:Int = emailHashingFunc(user.email)
         let email = user.email
@@ -63,6 +78,7 @@ class UserManager{
         FileManager.updateUserFile(userList, postList: postList)
     }
     
+    //해시테이블에서 이름으로 검색하여 유저를 리턴해주는 ㅎ마수
     static func findByName(name:String) -> User{
         let index:Int = emailHashingFunc(name)
         let noUser = User()
@@ -73,6 +89,7 @@ class UserManager{
         }
     }
     
+    //해시테이블 내에서 바이너리 서치 (이메일)
     static func emailBinarySearch(list:Array<User>, key:String)->Int{
         var first = 0
         var last = list.count
@@ -107,6 +124,7 @@ class UserManager{
         }
         return -(first+1)
     }
+    
     
     
 
